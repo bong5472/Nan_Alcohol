4864163a9935dabf77388fd334d682c378582c96
bong5472 <bong5472@naver.com>
2023-06-01 12:33:37 +0900

    train data mp4 reduce

--- reduce.py
+++ reduce.py
@@ -1,4 +1,5 @@
 import cv2
+import os
 
 input_file = 'C:/Users/User/Desktop/공모전/해커톤/video/input.mp4'
 
@@ -29,3 +30,63 @@ def reduce_video(input_file,number):
     cap.release()
     out.release()
     cv2.destroyAllWindows()
+    
+    
+def reduce_videoTime(input_file,number,start_time,output_path):
+    
+    end_time = start_time + 50
+    output_file = output_path + 'video_'+str(number)+'_'+str(start_time)+'.mp4'
+    cap = cv2.VideoCapture(input_file)
+    fps = cap.get(cv2.CAP_PROP_FPS)
+
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    out = cv2.VideoWriter(output_file, fourcc, fps, (width, height))
+
+    cap.set(cv2.CAP_PROP_POS_MSEC, start_time * 1000)
+
+    while True:
+        ret, frame = cap.read()
+
+        if not ret:
+            break
+
+        if cap.get(cv2.CAP_PROP_POS_MSEC) >= end_time * 1000:
+            break
+
+        out.write(frame)
+        # cv2.imshow('Trimmed Video', frame)       
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    cap.release()
+    out.release()
+    print(output_path+' // finish')
+    cv2.destroyAllWindows()
+    
+
+def get_files_in_folder(folder_path):
+    file_list = []
+    for root, dirs, files in os.walk(folder_path):
+        for file in files:
+            file_list.append(os.path.join(file))
+    return file_list
+
+folder_path = 'D:/hackaton_video/before'
+output_path = 'D:/hackaton_video/after/'
+files = get_files_in_folder(folder_path)
+
+num = 0
+for file in files:
+    num += 1
+    input_file = folder_path+'/'+file
+    print(file)
+    reduce_videoTime(input_file,num,0,output_path)
+    reduce_videoTime(input_file,num,50,output_path)
+    reduce_videoTime(input_file,num,100,output_path)
+    reduce_videoTime(input_file,num,150,output_path)
+    reduce_videoTime(input_file,num,200,output_path)
+    reduce_videoTime(input_file,num,250,output_path)
+    
+# os.system("shutdown /s /t 0")
