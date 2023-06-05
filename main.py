fde1b5dc6adf51536d025694e35aabc6fbd47c20
bong5472 <bong5472@naver.com>
2023-06-06 01:51:11 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import cv2
 import yolo1
 import openpose
+import tensorflow as tf
 
+model = tf.keras.models.load_model('C:/drunk.h5')
 def count_zero(arr):
     count = 0
     for i in arr:
@@ -10,25 +12,35 @@ def count_zero(arr):
     return count
 
 def main(video_path):
+    output_file = 'final_result.mp4'
+    fourcc = cv2.VideoWriter_fourcc(*'MP4V')
     cap = cv2.VideoCapture(video_path)
+    
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    frame_size = (frame_width, frame_height)
+    out = cv2.VideoWriter(output_file, fourcc, fps, frame_size)
+    
     frame_num = 0
-    data = []
+    data = [[]]
     result = 0
     drunken = False
+    tracking_data = [[0,0,0,0],[0,0,0,0],[0,0,0,0]]
     if not cap.isOpened:
         print('Video failed --- break')
         exit(0)
     
     while True:
+        print(frame_num)
         ret, frame = cap.read()
-        
         if frame is None:
             print('frame not detected --- break')
             break
         # drunk detect 전
         if not drunken:
             x,y,w,h = yolo1.detect(frame,frame_num)
-            
+            cv2.rectangle(frame,(int(x),int(y)),(int(x+w),int(y+h)),(0,255,0),2,1)
             if w == 0 and h == 0:
                 print('person not detected --- ')
                 
@@ -36,27 +48,51 @@ def main(video_path):
                 x_data,y_data = openpose.pose_save(frame,frame_num,x,y,w,h)
                 pose_data = x_data + y_data
                 # 포즈 데이터에 0이 많으면 제대로 인식이 안됬다고 가정
-                if count_zero(pose_data) >= 20:
-                    continue
-                else:
-                    data.append(pose_data)
+                if count_zero(pose_data) <= 10:
+                    data[0].append(pose_data)
             # 100프레임 고정
             if len(data) > 100:
-                data = data[1:]
-                result = model.predict(data).argmax(axis = 1)
+                data = [data[0][1:]]
+                result = model.predict(data)
+                print('pose :',result)
             elif len(data) == 100:
-                result = model.predict(data).argmax(axis = 1)
-            else:
-                continue
+                result = model.predict(data)
+                print('pose :',result)
             frame_num += 1
             # tracker 설정
-            if result == 1:
+            if result >= 0.5:
                 druken = True
-                tracker = tracker()
+                tracker = cv2.TrackerMIL_create()
                 isInit = tracker.init(frame,(x,y,w,h))
         # drunk detect 후
         else:
-            print()
-            
+            print('drunken')
+            ok, bbox = tracker.update(frame)
+            (x,y,w,h) = bbox
+            if ok:
+                cv2.rectangle(frame,(int(x),int(y)),(int(x+w),int(y+h)),(0,255,255),2,1)
+                cv2.putText(frame,'drunk',(x,y-10),1,0.5,(0,255,255),2,1)
+                tracking_data.append([x,y,w,h])
+            # 목표가 사라졌을 때
+            if tracking_data[-2] == tracking_data[-1] and tracking_data[-3] == tracking_data[-2]:
+                car_x,car_y,car_w,car_h = yolo1.detect_car(frame)
+                position_x, position_y = x + w/2, y + h/2
+                
+                if car_x <= position_x and position_x <= car_x + car_w and car_y <= position_y and position_y <= car_y + car_h:
+                    cv2.rectangle(frame,(int(car_x),int(car_y)),(int(car_x+car_w),int(car_y+car_h)),(0,0,255),2,1)    
+                    cv2.putText(frame,'Detected!!!',(x,y-10),1,0.8,(0,0,255),2,1)
+                else:
+                    drunken = False
+                    data = []
+                    result = 0
+                    tracking_data = [[0,0,0,0],[0,0,0,0],[0,0,0,0]]
+                    
+        cv2.imshow('drunk detecting...',frame)
+        out.write(frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break    
+    cap.release()
+    out.release()
+    cv2.destroyAllWindows()
+           
+main("C:/test2.mp4")
